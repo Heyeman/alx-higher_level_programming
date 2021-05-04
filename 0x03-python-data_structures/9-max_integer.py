9c03976c5328e4c2099215fc133bbfe698e5f291
Heyeman <abdisaheyeman083gmail.com>
2021-05-04 14:13:50 +0000

    new commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxVal = 0
-    for i in my_list:
-        if i > maxVal:
-            maxVal = i
-    return maxVal
+    if len(my_list) == 0:
+        return None
+    else:
+        maxVal = 0
+        for i in my_list:
+            if i > maxVal:
+                maxVal = i
+        return maxVal
