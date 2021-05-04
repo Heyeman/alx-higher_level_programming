1bef62ec73f4cf4e76cf398c550f33991a7fe50f
Heyeman <abdisaheyeman083gmail.com>
2021-05-04 14:30:39 +0000

    new commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,16 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
-            for j in matrix:
+            for j in matrix[i]:
                 print("{:d}".format(j), end=" ")
     print("")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
