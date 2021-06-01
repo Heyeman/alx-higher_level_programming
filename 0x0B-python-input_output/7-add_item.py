44f24b69e7b743a2bac5a3e29fa8ef7dde165ad1
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-06-01 18:28:58 +0300

    Add files via upload

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+# 9-add_item.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import json
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-"""Writes all command line input argument to json file."""
-
 
 if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
+
     try:
-        arg_list = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        arg_list = []
-    finally:
-        for i in range(1, len(sys.argv)):
-            arg_list.append(sys.argv[i])
-        save_to_json_file(arg_list, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
