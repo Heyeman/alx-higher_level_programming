add0323ac2f7ad15770409b60188087002230b08
Heyeman <59885488+Heyeman@users.noreply.github.com>
2021-09-14 22:43:05 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
-
-const req = require('request');
-const url = process.argv[2];
-req.get(url, function (err, res, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const data = JSON.parse(body);
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
     const completed = {};
-    for (const i of data) {
-      if (i.completed === true) {
-        if (i.userId in completed) {
-          completed[i.userId]++;
-        } else {
-          completed[i.userId] = 1;
-        }
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
+    });
     console.log(completed);
   }
 });
