17b4218ef2a1be1ff4a95cf05a5d4e75ec5dd93f
heyeman <abdisaheyeman083@gmail.com>
2022-02-22 06:53:12 +0300

    optimised"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+from sys import argv
+if len(argv) == 2:
+	print("1 argument:")
+	print("1: {}".format(argv[1]))
+else:
+	print("{} arguments:".format(len(argv)-1))
+	for i in len(argv[1:]):
+		print("{:d}: {:s}".format(i+1,argv[i+1]))
+		
+
+
+
+
+
+
+
+
+"""
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
@@ -10,3 +28,4 @@ if __name__ == "__main__":
         print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv) - 1):
             print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+"""
