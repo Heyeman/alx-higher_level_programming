78c31559cfa70d1b797f9cd78c6dabf2ba937d64
heyeman <abdisaheyeman083@gmail.com>
2022-02-22 07:00:42 +0300

    pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv
 if __name__ == "__main__":
 	if len(argv) == 2:
 		print("1 argument:")
-		print("1: {:d}".format(argv[1]))
+		print("1: {:s}".format(argv[1]))
 	else:
 		print("{:d} arguments:".format(len(argv)-1))
 		for i in range(len(argv[1:])):
