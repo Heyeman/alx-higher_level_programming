c8448819ae0eb707b3f4f560bdaf4f566ed2b8e2
heyeman <abdisaheyeman083@gmail.com>
2022-02-22 07:04:53 +0300

    optimised

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+	from sys import argv
+	print("{:d}".format(sum(argv[1:])))
+
+
+
+
+"""from sys import argv
 if __name__ == "__main__":
     x = 0
     for i in range(len(argv) - 1):
         x += int(argv[i + 1])
     print("{:d}".format(x))
+"""
